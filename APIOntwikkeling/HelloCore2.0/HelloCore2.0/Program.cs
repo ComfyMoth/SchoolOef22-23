c1b341e573ca3dbadb9bc91bd009cb5cf86ce2b1
Váli | Beetle <Mossybeetle@proton.me>
2023-11-09 20:42:13 +0100

    oke

---
+++ APIOntwikkeling/HelloCore2.0/HelloCore2.0/Program.cs
@@ -0,0 +1,10 @@
+namespace HelloCore2._0
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+    }
+}
