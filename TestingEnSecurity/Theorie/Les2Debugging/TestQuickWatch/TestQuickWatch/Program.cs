74442789ce333684272dacd86cafa14b40bccb91
ComfyMoth <Mossybeetle@proton.me>
2023-11-09 15:27:18 +0100

    niet crashen aub

---
+++ TestingEnSecurity/Theorie/Les2Debugging/TestQuickWatch/TestQuickWatch/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a, b;
+a = 1;
+b = 2;
+for (int i = 0; i < 10; i++)
+{
+    a = a + b;
+}
---
+++ TestingEnSecurity/Theorie/Les2Debugging/TestWatch/TestWatch/Program.cs
@@ -0,0 +1,11 @@
+﻿//// See https://aka.ms/new-console-template for more information
+int a, b, c;
+a = 1;
+b = 2; c = 0;
+for (int i = 0; i < 10; i++)
+{
+    a++;
+    b *= 2;
+    c = a + b;
+}
+return 0;
